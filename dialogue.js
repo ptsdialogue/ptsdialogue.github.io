e6113cf22a7b516d6a7fece397e111d5d4b92997
Sanjana Mendu <sm7gc@virginia.edu>
2021-02-09 10:25:38 -0500

    add daily check, favicon, and activities options

--- dialogue.js
+++ dialogue.js
@@ -4,58 +4,50 @@ var fixed_delay = 500; // TODO: adjust delay?
 var protocol;
 
 function loadMessage(protocol, i) { // recursive message display function
-
-    
     if (i >= Object.keys(protocol).length) { return; } // end recursion
+    if (protocol[i].message == "CLEAR") { clearChat(protocol, i); }
+    else { updateUI(protocol, i);}
+}
 
+function updateUI(protocol, i) {
     var curr = protocol[i];
-    
-    if (curr.message == "CLEAR") { // clear chat
-        $('.botui-messages-container').children().fadeOut(500)
-            .promise().then(function(){
-                $('.botui-messages-container').empty();
-                loadMessage(protocol, i + 1);
-            });
-    } else { // standard dialogue
-        botui.message.bot({
-            delay: Math.max(curr.message.length * 10, 500),
-            loading: true,
-            content: curr.message
-        })
-        .then(function () {
-            $(".botui-messages-container").stop().animate({ scrollTop: $(".botui-messages-container")[0].scrollHeight}, 500); // autoscroll bottom
-            if (curr.options) { // text-entry question
-                if (curr.options.length == 0) { 
-                    return botui.action.text({
-                        delay: fixed_delay,
-                        action: {
-                            placeholder: ''
-                        }
-                    }).then(function (res) { // TODO: handle user input
-                        // console.log(res.value);
+    botui.message.bot({
+        delay: Math.max(curr.message.length * 10, 500),
+        loading: true,
+        content: curr.message
+    })
+    .then(function () { // generic user input request
+        $(".botui-messages-container").stop().animate({ scrollTop: $(".botui-messages-container")[0].scrollHeight}, 500); // autoscroll to bottom
+        if (curr.options) {
+            if (curr.options.length == 0) { // text-entry question
+                return botui.action.text({
+                    delay: fixed_delay,
+                    action: {
+                        placeholder: ''
+                    }
+                }).then(function (res) {
+                    // TODO: handle user input
+                    loadMessage(protocol, i + 1);
+                });
+            } else { // multiple-choice question
+                var options = formatOptions(curr.options);
+                return botui.action.button({
+                    delay: fixed_delay,
+                    action: options
+                }).then(function (res) {
+                    if (curr.dialogue) { // load next dialogue
+                        var res_index = curr.options.indexOf(res.value);
+                        var json_script = curr.dialogue[res_index];
+                        $.getJSON("scripts/"+json_script,loadDialogue);
+                    } else { // continue
                         loadMessage(protocol, i + 1);
-                    });
-                } else { // multiple-choice question
-                    var options = formatOptions(curr.options);
-                    return botui.action.button({ 
-                        delay: fixed_delay,
-                        action: options
-                    }).then(function (res) {
-                        if (curr.dialogue) { // if redirect needed
-                            var res_index = curr.options.indexOf(res.value);
-                            var json_script = curr.dialogue[res_index];
-                            $.getJSON("scripts/"+json_script,loadDialogue);
-                        } else { // continue
-                            loadMessage(protocol, i + 1);
-                        }
-                    });
-                }
+                    }
+                });
             }
+        } else {
             loadMessage(protocol, i + 1);
-        });
-    }
-
-
+        }
+    });
 }
 
 function formatOptions(options) {
@@ -70,9 +62,14 @@ function formatOptions(options) {
 }
 
 function loadDialogue(data) {
-    console.log(data);
     protocol = data;
     loadMessage(protocol,0);
 }
 
-$.getJSON("scripts/init.json",loadDialogue);
+function clearChat(protocol, i) {
+    $('.botui-messages-container').children().fadeOut(500)
+        .promise().then(function(){
+            $('.botui-messages-container').empty(); 
+            loadMessage(protocol, i + 1);
+    });
+}
