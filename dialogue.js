ea995dee227d7bd0e8fd1b76bf64d6e499a4c9e2
Hee Jeong Han <heejeonghan@psu.edu>
2021-06-11 15:13:23 -0400

    updates

--- dialogue.js
+++ dialogue.js
@@ -99,22 +99,3 @@ function savePersona(protocol, i) { // preliminary function for saving persona c
     loadMessage(protocol, i + 1);
 }
 
-function countdown(minutes) {
-    var seconds = 60;
-    var mins = minutes
-    function tick() {
-        //This script expects an element with an ID = "counter". You can change that to what ever you want. 
-        var counter = document.getElementById("counter");
-        var current_minutes = mins-1
-        seconds--;
-        counter.innerHTML = current_minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
-        if( seconds > 0 ) {
-            setTimeout(tick, 1000);
-        } else {
-            if(mins > 1){
-                countdown(mins-1);           
-            }
-        }
-    }
-    tick();
-}
